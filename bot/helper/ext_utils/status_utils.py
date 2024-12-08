03ffbedbe16a4444c127ce6ae73bc09945532911
5hojib <yesiamshojib@gmail.com>
2024-12-08 19:39:00 +0600

    rev

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -212,7 +212,6 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
             MirrorStatus.STATUS_CONVERT,
             MirrorStatus.STATUS_FFMPEG,
             MirrorStatus.STATUS_QUEUEUP,
-            MirrorStatus.STATUS_SAMVID,
         ]:
             progress = (
                 await task.progress()
