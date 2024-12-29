aaa5ea6849b259f49a5159d243292ce64d109c82
5hojib <yesiamshojib@gmail.com>
2024-12-29 16:08:47 +0000

    Auto-format code [skip actions]

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -5,6 +5,7 @@ from re import match as re_match
 from aiofiles.os import path as aiopath
 
 from bot import LOGGER, bot_loop, task_dict_lock
+from bot.core.aeon_client import TgClient
 from bot.core.config_manager import Config
 from bot.helper.aeon_utils.access_check import error_check
 from bot.helper.ext_utils.bot_utils import (
@@ -13,7 +14,6 @@ from bot.helper.ext_utils.bot_utils import (
     get_content_type,
     sync_to_async,
 )
-from bot.core.aeon_client import TgClient
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 from bot.helper.ext_utils.links_utils import (
     is_gdrive_id,
