84b62a1de7c900a736e4e83b7205016b71dad152
5hojib <yesiamshojib@gmail.com>
2025-01-08 21:23:14 +0600

    update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -3,43 +3,43 @@ from bot.core.config_manager import Config
 i = Config.CMD_SUFFIX
 
 
-class _BotCommands:
+class BotCommands:
     def __init__(self):
-        self.StartCommand = f"start{i}"
-        self.MirrorCommand = [f"mirror{i}", f"m{i}"]
-        self.YtdlCommand = [f"ytdl{i}", f"y{i}"]
-        self.LeechCommand = [f"leech{i}", f"l{i}"]
-        self.YtdlLeechCommand = [f"ytdlleech{i}", f"yl{i}"]
-        self.CloneCommand = f"clone{i}"
-        self.MediaInfoCommand = f"mediainfo{i}"
-        self.CountCommand = f"count{i}"
-        self.DeleteCommand = f"del{i}"
-        self.CancelAllCommand = f"cancelall{i}"
-        self.ForceStartCommand = [f"forcestart{i}", f"fs{i}"]
-        self.ListCommand = f"list{i}"
-        self.SearchCommand = f"search{i}"
-        self.StatusCommand = f"status{i}"
-        self.UsersCommand = f"users{i}"
-        self.AuthorizeCommand = f"authorize{i}"
-        self.UnAuthorizeCommand = f"unauthorize{i}"
-        self.AddSudoCommand = f"addsudo{i}"
-        self.RmSudoCommand = f"rmsudo{i}"
-        self.PingCommand = f"ping{i}"
-        self.RestartCommand = f"restart{i}"
-        self.RestartSessionsCommand = f"restartses{i}"
-        self.StatsCommand = f"stats{i}"
-        self.HelpCommand = f"help{i}"
-        self.LogCommand = f"log{i}"
-        self.ShellCommand = f"shell{i}"
-        self.AExecCommand = f"aexec{i}"
-        self.ExecCommand = f"exec{i}"
-        self.ClearLocalsCommand = f"clearlocals{i}"
-        self.BotSetCommand = f"botsettings{i}"
-        self.UserSetCommand = f"settings{i}"
-        self.SpeedTest = f"speedtest{i}"
-        self.BroadcastCommand = [f"broadcast{i}", "broadcastall"]
-        self.SelectCommand = f"sel{i}"
-        self.RssCommand = f"rss{i}"
+        self.StartCommand = f"start{Config.CMD_SUFFIX}"
+        self.MirrorCommand = [f"mirror{Config.CMD_SUFFIX}", f"m{Config.CMD_SUFFIX}"]
+        self.YtdlCommand = [f"ytdl{Config.CMD_SUFFIX}", f"y{Config.CMD_SUFFIX}"]
+        self.LeechCommand = [f"leech{Config.CMD_SUFFIX}", f"l{Config.CMD_SUFFIX}"]
+        self.YtdlLeechCommand = [f"ytdlleech{Config.CMD_SUFFIX}", f"yl{Config.CMD_SUFFIX}"]
+        self.CloneCommand = f"clone{Config.CMD_SUFFIX}"
+        self.MediaInfoCommand = f"mediainfo{Config.CMD_SUFFIX}"
+        self.CountCommand = f"count{Config.CMD_SUFFIX}"
+        self.DeleteCommand = f"del{Config.CMD_SUFFIX}"
+        self.CancelAllCommand = f"cancelall{Config.CMD_SUFFIX}"
+        self.ForceStartCommand = [f"forcestart{Config.CMD_SUFFIX}", f"fs{Config.CMD_SUFFIX}"]
+        self.ListCommand = f"list{Config.CMD_SUFFIX}"
+        self.SearchCommand = f"search{Config.CMD_SUFFIX}"
+        self.StatusCommand = f"status{Config.CMD_SUFFIX}"
+        self.UsersCommand = f"users{Config.CMD_SUFFIX}"
+        self.AuthorizeCommand = f"authorize{Config.CMD_SUFFIX}"
+        self.UnAuthorizeCommand = f"unauthorize{Config.CMD_SUFFIX}"
+        self.AddSudoCommand = f"addsudo{Config.CMD_SUFFIX}"
+        self.RmSudoCommand = f"rmsudo{Config.CMD_SUFFIX}"
+        self.PingCommand = f"ping{Config.CMD_SUFFIX}"
+        self.RestartCommand = f"restart{Config.CMD_SUFFIX}"
+        self.RestartSessionsCommand = f"restartses{Config.CMD_SUFFIX}"
+        self.StatsCommand = f"stats{Config.CMD_SUFFIX}"
+        self.HelpCommand = f"help{Config.CMD_SUFFIX}"
+        self.LogCommand = f"log{Config.CMD_SUFFIX}"
+        self.ShellCommand = f"shell{Config.CMD_SUFFIX}"
+        self.AExecCommand = f"aexec{Config.CMD_SUFFIX}"
+        self.ExecCommand = f"exec{Config.CMD_SUFFIX}"
+        self.ClearLocalsCommand = f"clearlocals{Config.CMD_SUFFIX}"
+        self.BotSetCommand = f"botsettings{Config.CMD_SUFFIX}"
+        self.UserSetCommand = f"settings{Config.CMD_SUFFIX}"
+        self.SpeedTest = f"speedtest{Config.CMD_SUFFIX}"
+        self.BroadcastCommand = [f"broadcast{Config.CMD_SUFFIX}", "broadcastall"]
+        self.SelectCommand = f"sel{Config.CMD_SUFFIX}"
+        self.RssCommand = f"rss{Config.CMD_SUFFIX}"
 
 
-BotCommands = _BotCommands()
+# BotCommands = _BotCommands()
