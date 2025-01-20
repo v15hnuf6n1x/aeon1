850b07a281daba89e72f25a351818be34a313391
5hojib <yesiamshojib@gmail.com>
2025-01-20 09:57:17 +0600

    update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -243,8 +243,9 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
         if status == "All":
             return None, None
         msg = f"No Active {status} Tasks!\n\n"
-    buttons = ButtonMaker()
+    buttons = None
     if len(tasks) > STATUS_LIMIT:
+        buttons = ButtonMaker()
         msg += f"<b>Page:</b> {page_no}/{pages} | <b>Tasks:</b> {tasks_no} | <b>Step:</b> {page_step}\n"
         buttons.data_button("<<", f"status {sid} pre", position="header")
         buttons.data_button(">>", f"status {sid} nex", position="header")
@@ -255,7 +256,7 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
         for label, status_value in list(STATUSES.items()):
             if status_value != status:
                 buttons.data_button(label, f"status {sid} st {status_value}")
-    button = buttons.build_menu(8)
+    button = buttons.build_menu(8) if buttons else None
     msg += f"<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {get_readable_file_size(disk_usage(Config.DOWNLOAD_DIR).free)}"
     msg += f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {get_readable_time(time() - bot_start_time)}"
     return msg, button
