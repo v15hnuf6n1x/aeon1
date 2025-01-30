851ba1e4956db40fb31e61e9def249c813e5f2d1
5hojib <yesiamshojib@gmail.com>
2025-01-30 18:34:43 +0600

    update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -10,7 +10,7 @@ class BotCommands:
     JdMirrorCommand = [f"jdmirror{i}", f"jm{i}"]
     YtdlCommand = [f"ytdl{i}", f"y{i}"]
     LeechCommand = [f"leech{i}", f"l{i}"]
-    JdLeechCommand = [f"jdLeech{i}", f"jl{i}"]
+    JdLeechCommand = [f"jdleech{i}", f"jl{i}"]
     YtdlLeechCommand = [f"ytdlleech{i}", f"yl{i}"]
     CloneCommand = f"clone{i}"
     MediaInfoCommand = f"mediainfo{i}"
